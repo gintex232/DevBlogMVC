821703eb6730cce8510c4cc981b9be5193870a73
gintex232 <amahuo.godwin232@gmail.com>
2024-05-02 18:23:45 +0100

    .

---
+++ DevsTutorialCenterMVC/Utilities/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DevsTutorialCenterMVC.Utilities
+{
+    public static class Helper
+    {
+        public static string FormatDate(this DateTime date)
+        {
+            return date.ToString("ddd dd MMM yy");
+        }
+
+        public static string ShortenText(this string text, int length = 50)
+        {
+            var strippedText = Regex.Replace(text, "<.*?>", string.Empty);
+            return strippedText.Length < length ? $"{strippedText}..." : $"{strippedText[..length]}...";
+        }
+    }
+}
