628cc093cb41ee91e64c43740845449b6bc7ea21
gintex232 <amahuo.godwin232@gmail.com>
2024-05-02 18:33:28 +0100

    .

--- DevsTutorialCenterMVC/obj/Debug/net6.0/DevsTutorialCenterMVC.AssemblyInfo.cs
+++ DevsTutorialCenterMVC/obj/Debug/net6.0/DevsTutorialCenterMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevsTutorialCenterMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f388bd97b21a9726dc4143c6d37a6d1b87c5d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevsTutorialCenterMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevsTutorialCenterMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
